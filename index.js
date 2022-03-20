e5c9b1931bcffe18dcdde1c3ab2d068215e6f400
Samuel Oduba <samueloduba123@gmail.com>
2022-03-19 18:50:36 -0700

    neo bank

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const Joi = require('joi')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', function (request, response) {
+	return response.json({
+		status: 'Successful',
+		message: 'Request successful',
+		data: []
+	})
+})
+
+app.get('/api/get-virtual-accounts', function (request, response) {
+	return response.json({
+		status: 'Successful',
+		message: 'Request successful',
+		data: []
+	})
+})
+
+app.post('/api/create-virtual-account', function (request, response) {
+	const schema = Joi.object({
+		first_name: Joi.string().required(),
+		last_name: Joi.string().required(),
+		bvn: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
+		address: Joi.string().required(),
+		phone: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
+	})
+
+	const validation = schema.validate(request.body)
+	if(validation.error && validation.error.details.length > 0) {
+		return response.json({
+			status: 'Failed',
+			message: validation.error.details[0].message,
+			data: null
+		}, 400)
+	}
+
+	const accountNumber =  Math.floor(Math.random() * 9000000000) + 1000000000
+	return response.json({
+		status: 'Successful',
+		message: 'Account number generated successfully. An email has been sent to customer.',
+		data: {
+			account_number: accountNumber
+		}
+	})
+})
+
+const port = 3000
+console.log(`As i dey like this i cannot die until there is an error on port ${port}`)
+app.listen(port)
